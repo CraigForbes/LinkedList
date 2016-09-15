22784c19c5fb9c2bd31bd11388d1e5b1c8cba3dd
Craig <craigforbes@outlook.com>
2016-09-15 17:53:47 +0200

    LinkedList Solution added

---
+++ LinkedList/LinkedListSolution.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    public class LinkedListSolution
+    {
+        public int Solution(IntList L)
+        {
+            // cant ForEach, grrrr
+
+            // stores our count to be returned
+            int listCount = 0;
+
+            // the current list item to check
+            var intList = L;
+
+            // flag to see if there are more items
+            bool hasMore = true;
+
+            //var item = L;
+            //while (item.Next != null)
+            //{
+            //    listCount++;
+            //    item = item.Next;
+            //}
+
+            do
+            {
+                if (intList != null)
+                {
+                    listCount += 1;
+                    intList = intList.Next;
+                }
+                else
+                {
+                    hasMore = false;
+                }
+            } while (hasMore);
+
+
+            return listCount;
+        }
+    }
+
+    public class IntList
+    {
+        public int Value { get; set; }
+        public IntList Next { get; set; }
+    }
+}
---
+++ LinkedListTests/LinkedListTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LinkedList;
+
+namespace LinkedListTests
+{
+    [TestClass]
+    public class LinkedListTests
+    {
+        [TestMethod]
+        public void BasicTest()
+        {
+            var list = CreateIntList(new[] { 4, 7, 5, 1, 8, 2 });
+            var solution = new LinkedListSolution();
+            var result = solution.Solution(list);
+
+            Assert.AreEqual(6, result);
+        }
+
+        private IntList CreateIntList(int[] values)
+        {
+            var head = new IntList();
+            var current = head;
+            IntList previous = null;
+            foreach (var value in values)
+            {
+                current.Value = value;
+                if (previous != null)
+                {
+                    previous.Next = current;
+                }
+                previous = current;
+                current = new IntList();
+            }
+            return head;
+        }
+    }
+}
